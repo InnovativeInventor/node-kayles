43a0f482d083c033f7f47f1ccea2b7e20b585b81
InnovativeInventor <root@max.fan>
2021-04-03 10:52:14 -0400

    Improve formatting

--- format.py
+++ format.py
@@ -1,11 +1,19 @@
 data = eval(input()) # unsafe
 n = 8
 
-for i in range(n):
-    print()
-    for j in range(n):
-        if (i,j) in data:
-            print(data.get((i,j)), end = " ")
-        else:
-            print("  ", end="")
+for i in range(n+1):
+    if i == 0:
+        print("  ", end="")
+        for j in range(n):
+            print(j, end = " ")
+        print()
+        for j in range(n):
+            print("_", end = " ")
+    else:
+        print("\n" + str(i-1), end= "|")
+        for j in range(n):
+            if (i-1,j) in data:
+                print(data.get((i-1,j)), end = " ")
+            else:
+                print("  ", end="")
 
