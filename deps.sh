d215a0174a75e026d46be0082a2eb8c70080c794
InnovativeInventor <root@max.fan>
2021-04-03 14:32:09 -0400

    Fixed improper dep

--- deps.sh
+++ deps.sh
@@ -1,2 +1,6 @@
+# basics
 sudo apt-get update
-sudo apt-get install gcc build-essentials
+sudo apt-get install -y gcc build-essential
+
+# install rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
