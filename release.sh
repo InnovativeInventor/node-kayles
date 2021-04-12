fb6d0952a2ab9465f2e026c6f19be4c88b3cc9ec
InnovativeInventor <root@max.fan>
2021-04-12 15:03:37 -0400

    Switch back to native compile

--- release.sh
+++ release.sh
@@ -1,5 +1,5 @@
-# RUSTFLAGS="-C target-cpu=native" cargo build --release
+RUSTFLAGS="-C target-cpu=native" cargo build --release
 # RUSTFLAGS="--target=x86_64-pc-windows-msvc" cargo build --release
-RUSTFLAGS="--target=x86_64-pc-windows-gnu" cargo build --release
-RUSTFLAGS="--target=x86_64-apple-darwin" cargo build --release
-RUSTFLAGS="--target=x86_64-unknown-linux-gnu" cargo build --release
+# RUSTFLAGS="--target=x86_64-pc-windows-gnu" cargo build --release
+# RUSTFLAGS="--target=x86_64-apple-darwin" cargo build --release
+# RUSTFLAGS="--target=x86_64-unknown-linux-gnu" cargo build --release
